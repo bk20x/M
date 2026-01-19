4b6bcef4f9575407c34b21403ccb04aec3dc35f3
bk20x <bkaenel4@gmail.com>
2026-01-19 17:52:40 -0600

    Revert "add more table examples"
    This reverts commit bd4a55a253450d315377f8a9c8339daefcecf9d3.

--- examples/tableutils.m
+++ examples/tableutils.m
@@ -6,27 +6,3 @@
       `(each (,k (tableKeys ,table))
 	,body)))
 
-
-(macro pairs (binding body)
- (let ((k     (car binding))
-       (v     (car (cdr binding)))
-       (table (car (cdr (cdr binding))))) 
-    `(each (,k (tableKeys ,table)) 
-      (let ((,v (get ,k ,table)))
-        ,body))))
-
-
-(define mapvs (-> (table fn) 
-  (let ((result {})) 
-    (pairs (k v table) (put k (fn v) result))
-  result)))
-
-(define filtervs (-> (table pred)
- (let ((result {})) 
-   (pairs (k v table) 
-     (if (pred v) (put k v result)))
-  result)))
-
-
-(define consume (-> (src dest)
- (let () (pairs (k v src) (put k v dest)) dest)))
