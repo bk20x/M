f5e50e8320f4d280724e09ed53efc7072b428ae9
bk20x <bkaenel4@gmail.com>
2026-02-09 21:07:49 -0600

    add example for mutating and inspecting functions

---
+++ examples/mutable_lambdas.m
@@ -0,0 +1,26 @@
+'(in M lambdas are mutable and inspectable basically to the extent of any other data
+  M provides primitives "body" and "lparams" which return a reference to the lambdas body and parameters respectively.
+  as well as "setb" and "setp" for setting the parameters and body directly)
+
+
+(open Strings)
+
+(define f (-> (x) (* x x)))
+(echo (f 10))
+
+(echo (fmt "params = $ body = $" (lparams f) (body f)))
+
+(echo (fmt "f before mutation=$" f))
+(setq (car (body f)) +)
+(echo (fmt "f after mutation=$" f))
+(echo (f 10))
+
+(setp f nil)
+(setb f '(echo "Fuck you!, I dont do any kind of math anymore!"))
+(echo (fmt "someone hacked the system and mutated our function again $" f))
+(f)
+
+
+
+
+
